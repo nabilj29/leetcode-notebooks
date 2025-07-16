3bf3e9af05b8bf6ea3b0eeba51663ef6d15a9d3a
Nabil Johny <johnyn@mcmaster.ca>
2025-07-16 15:07:26 -0400

    trapping rain water

---
+++ TwoPointers/sortColors.py
@@ -0,0 +1,3 @@
+#Write a function to sort a given integer array nums in-place (and without the built-in sort function), 
+# where the array contains n integers that are either 0, 1, and 2 and represent the colors red, white, and blue. 
+# Arrange the objects so that same-colored ones are adjacent, in the order of red, white, and blue (0, 1, 2).
